6f41eda50fc0d73dbd45748649a997817511d209
Cole <colejackowski@gmail.com>
2013-12-01 15:28:38 -0500

    Added geoJSON polygon layer and search box

--- web/mapbox/js/map.js
+++ web/mapbox/js/map.js
@@ -1,4 +1,27 @@
-// Initialize map
-var map = L.mapbox.map('map', 'pichot.gdo5mg5o', {
-  detectRetina: true
-});
+window.onload = init;
+
+function init() {
+
+	// Initialize map
+	var map = L.mapbox.map('map', 'pichot.gdo5mg5o', {
+  		detectRetina: true
+	});
+
+	// Add geoJSON layer
+	L.geoJson(parcels, {
+		onEachFeature: bindInfo // Call this function on each geoJSON feature
+	}).addTo(map);
+	
+	// Add search box
+	new L.Control.GeoSearch({
+        provider: new L.GeoSearch.Provider.Google(),
+        position: 'topcenter'
+    }).addTo(map);
+}
+
+// Attach an informational popup to each polygon
+function bindInfo(feature, layer) {
+	layer.bindPopup('Address: ' + feature.properties.Address + '<br>' +
+					'City: ' + 	  feature.properties.City
+	);
+}
