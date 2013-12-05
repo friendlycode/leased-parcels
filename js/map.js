4f60ded5e79e0aeca74bf5ed7b3f7c9f5494fe22
Brian Ryckbost <bryckbost@gmail.com>
2013-12-05 08:55:07 -0500

    Add a legend control

--- js/map.js
+++ js/map.js
@@ -10,13 +10,13 @@ function init() {
 	// Add geoJSON layer for private parcels
 	L.geoJson(parcels, {
 		onEachFeature: bindPrivateInfo, // Call this function on each geoJSON feature
-		style: { 'color': '#FF0000' }
+		style: { 'color': parcelColors["private"] }
 	}).addTo(map);
 	
 	// Add geoJSON layer for state parcels
 	L.geoJson(state_parcels, {
 		onEachFeature: bindStateInfo,
-		style: { 'color': '#B31453' }
+		style: { 'color': parcelColors["state"] }
 	}).addTo(map);
 	
 	// Add search box
@@ -24,6 +24,19 @@ function init() {
         provider: new L.GeoSearch.Provider.Google(),
         position: 'topcenter'
     }).addTo(map);
+
+  // Add legend
+  var legend = L.control({position: 'bottomright'});
+  legend.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'info legend'), labels = [];
+
+		labels.push('<i style="background:' + parcelColors["private"] + '"></i> ' + 'private parcels');
+		labels.push('<i style="background:' + parcelColors["state"] + '"></i> ' + 'state parcels');
+
+		div.innerHTML = labels.join('<br>');
+		return div;
+  };
+  legend.addTo(map);
 }
 
 // Attach an informational popup to each polygon
@@ -35,4 +48,10 @@ function bindPrivateInfo(feature, layer) {
 function bindStateInfo(feature, layer) {
 	layer.bindPopup('Description: ' + feature.properties.ownerdesc + '<br>' +
 					'Lesee Name: ' + feature.properties.vn_leseenm );
+}
+
+// get color depending on parcel type
+parcelColors = {
+  "private": "#FF0000",
+  "state": "#B31453"
 }
